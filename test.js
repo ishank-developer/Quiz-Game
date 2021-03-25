67635ebea797119abb9bec9389441992c09d25bd
ishank-developer <ishuaggarwal196@gmail.com>
2021-03-26 02:47:43 +0530

    fg

---
+++ test.js
@@ -0,0 +1,12 @@
+
+function beginQuiz(id) {
+    alert("");
+    location.href='./Quiz.html'
+}
+
+ function startQuiz(difficulty) {
+let jsonData = require('./test.json');
+
+console.log(jsonData);
+    
+}
